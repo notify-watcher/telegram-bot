db803c0ad9c9f3c58590ab0c95caa2484287ff1a
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-29 18:00:54 -0300

    docker build run push scripts

--- scripts/docker_image.sh
+++ scripts/docker_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "docker.pkg.github.com/notify-watcher/telegram-bot/telegram-bot:$(git describe)"
+echo "docker.pkg.github.com/notify-watcher/telegram-bot/telegram-bot"
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 image=$(scripts/docker_image.sh)
+tag=$(scripts/docker_tag.sh)
 
-docker push $image
-   
+docker push $image:$tag
+docker push $image:latest
+
