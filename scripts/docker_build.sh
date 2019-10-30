c1a1a9d0e871e88461dad84b03fab88cfc4bf32c
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-30 19:24:33 -0300

    chore(ci): remove comments from docker_build docker_push

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,7 +3,6 @@
 image=$(scripts/docker_image.sh)
 tag=$(scripts/docker_tag.sh)
 
-  # -t $image:latest \
 DOCKER_BUILDKIT=1 docker build \
   -t $image:$tag \
   --progress=auto \
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -3,6 +3,4 @@
 image=$(scripts/docker_image.sh)
 tag=$(scripts/docker_tag.sh)
 
-# docker push $image:latest
 docker push $image:$tag
-
