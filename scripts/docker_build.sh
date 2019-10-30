03bd2e2f84d0fbb1da0f0f1571483ffe93ad9abe
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-30 19:09:19 -0300

    chore(ci): typo in docker_build

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,8 +3,8 @@
 image=$(scripts/docker_image.sh)
 tag=$(scripts/docker_tag.sh)
 
+  # -t $image:latest \
 DOCKER_BUILDKIT=1 docker build \
-  \ # -t $image:latest \
   -t $image:$tag \
   --progress=auto \
   --secret id=npmrc,src=$HOME/.npmrc \
